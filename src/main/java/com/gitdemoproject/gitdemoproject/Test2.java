a68f43686e0f5641a970fe13555bbdf255c59529
farhan.ahmad <farhan.ahmad@IT-04.ktech.local>
2020-11-25 15:40:43 +0500

    commit test 2 to branchX file

---
+++ src/main/java/com/gitdemoproject/gitdemoproject/Test2.java
@@ -0,0 +1,13 @@
+package com.gitdemoproject.gitdemoproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World2!" );
+    }
+}
