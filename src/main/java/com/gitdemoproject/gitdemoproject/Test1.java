8f8cc25bb74ca7532241864b01a6b5343bf54c34
farhan.ahmad <farhan.ahmad@IT-04.ktech.local>
2020-11-25 15:08:21 +0500

    First Commit....

---
+++ src/main/java/com/gitdemoproject/gitdemoproject/Test1.java
@@ -0,0 +1,13 @@
+package com.gitdemoproject.gitdemoproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
